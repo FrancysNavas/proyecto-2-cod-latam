b6b7ab16c70a38a931c19d6afb09896f2a813741
francysNavas <francys.navas.sanchez@gmail.com>
2023-07-27 21:56:36 -0300

    codigo para el boton CV

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+/** accion del boton CV */
+const button = document .querySelector(".button");
 
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+        button.classList.add("animate");
+
+        setTimeout(() => {
+            button.classList.remove("animate");
+    }, 600)
+});
+ 
